90df6cc9e1ce5e21fc0446aa10d720d8e48d1d6a
MicheleSica <michele.sica@terradue.com>
2019-07-25 19:02:10 +0200

    fixed double data filed issue produced by recast

--- src/main/app-resources/lib/util.sh
+++ src/main/app-resources/lib/util.sh
@@ -422,7 +422,7 @@ function create_interf_properties()
     local propfile="${inputfile}.properties"
     echo "title = DIAPASON InSAR Sentinel-1 TOPSAR(IW,EW) - ${description} - ${datestart} ${dateend}" > "${propfile}"
     echo "Description = ${description}" >> "${propfile}"
-    echo "Date = ${datestart}/${dateend} " >> "${propfile}"
+    echo "date = ${datestart}/${dateend} " >> "${propfile}"
     local sensor=`grep -h "SENSOR NAME" "${geosarm}" | cut -b 40-1024 | awk '{print $1}'`
     echo "Sensor\ Name = ${sensor}" >> "${propfile}"
     local masterid=`head -1 ${serverdir}/masterid.txt`
